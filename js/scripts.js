391d72336f8b199c5cbe5c35c574b1de8a494866
informaticaesne <daniihrr@gmail.com>
2023-11-02 21:48:10 +0100

    Work_layout_hover_02(functional)

--- js/scripts.js
+++ js/scripts.js
@@ -8,8 +8,11 @@ $(document).ready(function() {
         //console.log("hover col");
         
         var work_number = $(this).data('work');
-        console.log('Hover en col ' + work_number);
+        //console.log('Hover en col ' + work_number);
+        console.log("img/origen" + work_number + ".jpg");
 
+        $("#work_img").attr("src","media/img/origen" + work_number + ".jpg");
+       
     }, function() {
         //pantalla.remove();
     });
