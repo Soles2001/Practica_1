ffcccb35d1be29a766e3ccfb658a28d8f7b7d519
daniihrr <daniihrr@gmail.com>
2023-11-03 17:57:55 +0100

    Menu_nav_y_header_works_funcional_03

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,15 @@ $(document).ready(function() {
     console.log("jQuery load");
 
 
+    // Agrega una imagen (col 1) por defecto cuando la página se carga
+    $("#work_img").attr("src", "media/img/origen1.jpg");
+
     $('._row_header .col').hover(function() {
         // Cuando haces hover en una columna
-        
         //console.log("hover col");
-        
-        var work_number = $(this).data('work');
-        //console.log('Hover en col ' + work_number);
-        console.log("img/origen" + work_number + ".jpg");
 
+        var work_number = $(this).data('work');
+        //console.log("img/origen" + work_number + ".jpg");
         $("#work_img").attr("src","media/img/origen" + work_number + ".jpg");
        
     }, function() {
